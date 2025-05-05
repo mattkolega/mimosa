21dd293425c66d457ca1abf09a71e201848f60f6
Matthew Kolega <12107095+mattkolega@users.noreply.github.com>
2025-05-05 19:14:04 +1000

    Setup database for use in frontend

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,4 +1,20 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use tauri_plugin_sql::{Migration, MigrationKind};
+
+const MIGRATION_V1: &str = "
+    CREATE TABLE track (
+        id INTEGER PRIMARY KEY,
+        title TEXT NOT NULL,
+        artist TEXT,
+        album TEXT,
+        filepath TEXT NOT NULL
+    );
+
+    CREATE TABLE location (
+        id INTEGER PRIMARY KEY,
+        path TEXT NOT NULL
+    );
+";
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
@@ -6,7 +22,20 @@ fn greet(name: &str) -> String {
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    let migrations = vec![Migration {
+        version: 1,
+        description: "create_initial_tables",
+        sql: MIGRATION_V1,
+        kind: MigrationKind::Up,
+    }];
+
     tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())
+        .plugin(
+            tauri_plugin_sql::Builder::new()
+                .add_migrations("sqlite:mimosa.db", migrations)
+                .build(),
+        )
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
